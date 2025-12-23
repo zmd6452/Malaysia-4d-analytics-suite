dbd3566d51e7721aa7ffa1a215dd8a6e57dae6c8
Zul <rukia6452@gmail.com>
2025-12-23 08:24:26 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// YOU CAN EDIT EVERYTHING HERE
+const LOGIC_CONFIG = {
+  mustIncludeDigits: ["1", "2"],   // <- change anytime
+  totalPicks: 4,
+  allowRepeatDigit: true,
+  numberLength: 4,
+  minFrequency: 2,
+  allowedDigits: ["0","1","2","3","4","5","6","7","8","9"]
+};
